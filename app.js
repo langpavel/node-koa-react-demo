4849f7f7cbcbda8d5ee07645f852c7d6e037c331
Pavel Lang <langpavel@maternacz.com>
2014-03-10 22:01:50 +0100

    X-Response-Time middleware in nanosecond precision

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var koa = require('koa');
 var app = koa();
 
 
+// x-response-time
+app.use(function *xResponseTime(next){
+  var start = process.hrtime();
+  yield next;
+  var diff = process.hrtime(start);
+  this.set('X-Response-Time', (diff[0] * 1e3 + diff[1] / 1e6) + 'ms');
+});
+
+
 app.use(function *() {
 	this.body = 'Hello World!';
 });
