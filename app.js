2bc910ac485cf83a8dc2e4e24e696ef40e2d55c0
Pavel Lang <langpavel@maternacz.com>
2014-03-11 21:54:25 +0100

    Cookies

--- app.js
+++ app.js
@@ -5,17 +5,33 @@ var koa = require('koa');
 var app = koa();
 
 
+// see https://github.com/koajs/koa/blob/master/docs/api/index.md#appkeys
+app.keys = ['new key', 'old key'];
+
+
 // x-response-time
 app.use(function *xResponseTime(next){
   var start = process.hrtime();
-  yield next;
+  yield next; // process next middleware
   var diff = process.hrtime(start);
   this.set('X-Response-Time', (diff[0] * 1e3 + diff[1] / 1e6) + 'ms');
 });
 
 
-app.use(function *() {
-	this.body = 'Hello World!';
+app.use(function *(next) {
+  // only for homepage
+  if ('/' != this.path) return next;
+
+  // see https://github.com/jed/cookies#cookiesget-name--options--
+  var clientRequestCount = this.cookies.get('counter', { signed: true }) || 1;
+
+  this.body = 'Hello World!\n';
+  this.body += 'Client request count: ' + clientRequestCount + '\n';
+
+  clientRequestCount++;
+
+  // see https://github.com/jed/cookies#cookiesset-name--value---options--
+  this.cookies.set('counter', clientRequestCount, { signed: true });
 });
 
 
